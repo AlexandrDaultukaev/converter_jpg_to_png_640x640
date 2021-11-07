a2f94f3537adcf7e513e2888f4022d47d8ed3a45
AlexandrDaultukaev <shyra31102000@mail.ru>
2021-11-07 16:23:57 +0700

    Add formatting func

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # This is a sample Python script.
 from PIL import Image
+from typing import Tuple, List
 import os
 
-def resize_image(input_image_path,
-                 output_image_path,
-                 size):
+
+def resize_image(input_image_path: str,
+                 output_image_path: str,
+                 size: Tuple[int, int]) -> None:
     original_image = Image.open(input_image_path)
     width, height = original_image.size
 
@@ -13,13 +15,46 @@ def resize_image(input_image_path,
     resized_image.save(output_image_path)
 
 
-for i in range(1, 101):
-    im = Image.open(f'../Fake/{i}.jpg')
-    im.save(f'../Fake_png/{i}.png')
+def list_files(folder: str) -> List[str]:
+    num = [f for f in os.listdir(folder)
+           if os.path.isfile(os.path.join(folder, f))]
+    return num
 
-for i in range(1, 101):
-    resize_image(input_image_path=f'../Fake_png/{i}.png',
-                 output_image_path=f'../Fake_png_640/{i}.png',
-                 size=(640, 640))
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def formatting() -> None:
+    folder_from = input("Введите папку, откуда брать изображения?\n")
+    folder_to = input("Введите название папки, куда складывать изображения?\n")
+
+    if not os.path.exists(folder_from):
+        print("Такой папки не существует\n")
+
+    try:
+        os.mkdir(folder_to)
+    except FileExistsError:
+        pass
+
+    try:
+        os.mkdir("tmp")
+    except FileExistsError:
+        pass
+
+    files = list_files(folder_from)
+
+    for i in range(len(files)):
+        os.rename(f"{folder_from}/{files[i]}", f"{folder_from}/{i}.{files[i].split('.')[1]}")
+
+    for i in range(len(files)):
+        im = Image.open(f"{folder_from}/{i}.{files[i].split('.')[1]}")
+        im.save(f'tmp/{i}.png')
+
+    for i in range(len(files)):
+        resize_image(input_image_path=f'tmp/{i}.png',
+                     output_image_path=f'{folder_to}/{i}.png',
+                     size=(640, 640))
+
+    for i in range(len(files)):
+        os.remove(f'tmp/{i}.png')
+    os.rmdir("tmp")
+
+
+formatting()
